ab0b9bfe40c726962007dc95b4b12a9ad8b4bfef
Dan Scott <dscott@laurentian.ca>
2013-05-30 22:51:27 -0400

    Add in year and collection facets
    The collection facets might not turn out to be good enough, as it
    provides no distinction between Reaction and L'Orignal. This might lead
    to having to provide more granularity in our data store (perhaps
    creating a new column based on the titles?). In any case, we now have
    year and collection facets available to be added to the display.
    
    Signed-off-by: Dan Scott <dscott@laurentian.ca>

--- restapp/restapp.py
+++ restapp/restapp.py
@@ -25,7 +25,24 @@ DB = psycopg2.connect(
 @app.route("/search/<query>/<int:limit>")
 @app.route("/search/<query>/<int:limit>/<int:page>")
 def search(query, limit=10, page=0):
+    """Return JSON formatted search results, including snippets and facets"""
+
+    results = __get_ranked_results(query, limit, page)
+    years = __get_year_facet(query)
+    collections = __get_collection_facet(query)
+
+    resj = json.dumps({
+        'query': query,
+        'results': results,
+        'years': years,
+        'collections': collections
+    })
+    response = flask.Response(response="%s" % resj, mimetype='application/json')
+    return response
+
+def __get_ranked_results(query, limit, page):
     """Simple search for terms, with optional limit and paging"""
+
     sql = """
         WITH q AS (SELECT plainto_tsquery(%s) AS query),
         ranked AS (
@@ -38,6 +55,7 @@ def search(query, limit=10, page=0):
         FROM ranked, q
         ORDER BY ranked DESC
     """
+
     cur = DB.cursor()
     cur.execute(sql, (query, limit, page*limit))
     results = []
@@ -50,9 +68,51 @@ def search(query, limit=10, page=0):
             'year': row[4],
             'snippets': row[5].split('||||')
         })
-    resj = json.dumps({'query': query, 'results': results})
-    response = flask.Response(response="%s" % resj, mimetype='application/json')
-    return response
+    cur.close()
+
+    return results
+
+
+def __get_year_facet(query):
+    """Gather counts, by year, of matching results"""
+    sql = """
+        SELECT year, COUNT(year)
+        FROM items
+        WHERE plainto_tsquery(%s) @@ tsv
+        GROUP BY year
+        ORDER BY 2 DESC
+    """
+
+    cur = DB.cursor()
+    cur.execute(sql, (query,))
+    years = []
+    for row in cur:
+        years.append([
+            row[0],
+            row[1]
+        ])
+
+    return years
+
+def __get_collection_facet(query):
+    """Gather counts, by collection, of matching results"""
+    sql = """
+        SELECT collection, COUNT(collection)
+        FROM items
+        WHERE plainto_tsquery(%s) @@ tsv
+        GROUP BY collection
+        ORDER BY 2 DESC
+    """
+    cur = DB.cursor()
+    cur.execute(sql, (query,))
+    collections = []
+    for row in cur:
+        collections.append([
+            row[0],
+            row[1]
+        ])
+
+    return collections
 
 if __name__ == "__main__":
     app.debug = True
